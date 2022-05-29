a225017a24a53f48fadef5370c42fe0088d1b399
Yoly Cordova <yolycordova2@gmail.com>
2022-05-29 11:03:11 -0300

    formulario new

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 function btn_enviar() {
     //Valido el Nombre
   if (document.form.nombre.value.length == ""){
-    alert("Escribe tu nombre")
+    alert("Escribe tu nombre");
       document.form.nombre.focus()
       return false;
   } 
   //Valido el Apellido
   if (document.form.apellido.value.length == ""){
-    alert("Escribe tu apellido")
+    alert("Escribe tu apellido");
       document.form.apellido.focus()
       return "";
   }
   //Valido el Email
 
   if (document.form.correo.value.length == ""){
-     alert("Escribe tu mail")
+     alert("Escribe tu mail");
       document.form.correo.focus()
      return "";
   }
 
   //Valido el Telefono
   if (document.form.tlf.value.length == 0){
-    alert("Escribe tu Teléfono")
+    alert("Escribe tu Teléfono");
       document.form.tlf.focus()
       return 0;
   }
   //Valido el País
   if (document.form.paises.selectedIndex == 0) {
-    alert("Seleccione su país")
+    alert("Seleccione su país");
     document.form.paises.focus()
     return 0;
   }
   //Valido la provincia
   if (document.form.provincias.selectedIndex == 0) {
-    alert("Seleccione su provincia")
+    alert("Seleccione su provincia");
     document.form.provincias.focus()
     return 0;
   }
